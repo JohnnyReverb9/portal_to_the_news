cafa4728a03b057d224e43236ae3fbaa8c22fdf8
Stepan <stepan.rulyov@gmail.com>
2024-07-27 11:40:12 +0300

    Сделан вывод главной странички

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
 func main() {
-	// ...
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	serveMux := http.NewServeMux()
+	serveMux.HandleFunc("/", indexHandler)
+
+	log.Println("Begin listening on http://localhost:" + port)
+
+	err := http.ListenAndServe(":"+port, serveMux)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("<h1>Welcome!</h1>"))
+
+	// log.Println(r.Method, r.Response.StatusCode)
+
+	if err != nil {
+		log.Println(err)
+	}
 }
